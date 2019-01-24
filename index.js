634f982895a7c079d536762e100ffaa1a6182e81
kendall willard <kendallwillard@hotmail.com>
2019-01-24 22:51:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
-function sayHiToGrandaa(string) {
+function sayHiToGrandma(string) {
   if(string === 'hello') {
     return "I can\'t hear you!"
 
